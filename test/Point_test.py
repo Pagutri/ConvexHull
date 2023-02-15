1594eef39629cd5d187122c06f9eed95bf5f0310
Salma Gutierrez <pagutri@gamil.com>
2023-02-15 16:36:56 -0600

    Create test for Class Point

---
+++ test/Point_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class Point_test(unittest.TestCase):
+    def test_point_class_works(self):
+        my_point = Point(0.1, 16)
+        self.assertEqual(0.1, my_point.x)
+        self.assertEqual(16, my_point.y)
+
+if __name__ == '__main__':
+    unittest.main()
