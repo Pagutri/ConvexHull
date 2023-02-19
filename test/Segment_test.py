99f26a163fa087289badd3c1741554c6e6a0bcd8
Pa Gutri <pagutri@gmail.com>
2023-02-19 13:39:43 -0600

    Correct error in Segment test

--- test/Segment_test.py
+++ test/Segment_test.py
@@ -8,7 +8,7 @@ class Segment_test(unittest.TestCase):
     def test_segment_class_works(self):
     	p1 = Point(1.0, 2.0)
     	p2 = Point(3.4, 6)
-    	my_segment = Segment(p1, p1)
+    	my_segment = Segment(p1, p2)
     	self.assertEqual(1.0, my_segment.p1.x)
     	self.assertEqual(2.0, my_segment.p1.y)
     	self.assertEqual(3.4, my_segment.p2.x)
