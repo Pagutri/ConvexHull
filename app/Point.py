f03cc8a5aa99161a12413a18c4686001a977baee
Pa Gutri <pagutri@gmail.com>
2023-02-19 22:27:19 -0600

    Me quedé en ordenar por ángulo polar. Falta probar

--- app/Point.py
+++ app/Point.py
@@ -1,5 +1,53 @@
+from math import sqrt, acos, atan
+
 class Point():
+    """2D Points"""
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def __sub__(self, p):
+        return Point(self.x - p.x, self.y - p.y)
+
+    def dot_product(p1, p2):
+        return p1.x*p2.x + p1.y*p2.y
+
+    def norm(self):
+        return sqrt(self.x**2 + self.y**2)
+
+    def polar_angle(self):
+        """Angle of the vector that points to self
+        with respect to the origin"""
+        return atan(self.y / self.x)
+    
+    def angle_between_segments(p1, p2, p3):
+        """Angle between segments p2p1 and p2p3"""
+        # Translate segments to the origin
+        p1_prim = p1 - p2
+        p3_prim = p3 - p2
+        numerator = Point.dot_product(p1_prim, p3_prim)
+        denominator = p1_prim.norm() * p3_prim.norm()
+        return acos(numerator / denominator)
+
+    def min_y_coordinate(set_of_points):
+        min_y = set_of_points[0].y
+        index_of_min = 0
+
+        for i in range(len(set_of_points)):
+            if(set_of_points[i].y < min_y):
+                min_y = set_of_points[i].y
+                index_of_min = i
+            elif(set_of_points[i].y == min_y \
+                and set_of_points[i].x < set_of_points[index_of_min].x):
+                min_y = set_of_points[i].y
+                index_of_min = i
+
+        return index_of_min
+
+    def graham_scan(set_of_points):
+        """Convex hull"""
+        i = Point.min_y_coordinate(set_of_points)
+        p0 = set_of_points[i]
+        del set_of_points[i]
+        set_of_points.sort(key = lambda x: (x - p0).polar_angle())
+        return set_of_points
--- test/Point_test.py
+++ test/Point_test.py
@@ -9,5 +9,49 @@ class Point_test(unittest.TestCase):
         self.assertEqual(0.1, my_point.x)
         self.assertEqual(16, my_point.y)
 
+    def test_sub_method(self):
+        p2 = Point(5.0, 2.0)
+        p3 = Point(3.5, 6.0)
+        diff = p2 - p3
+        self.assertEqual(1.5, diff.x)
+        self.assertEqual(-4.0, diff.y)
+
+    def test_dot_product_method(self):
+        p1 = Point(2.0, 1.0)
+        p2 = Point(5.0, 2.0)
+        p3 = Point(3.5, 6.0)
+        self.assertEqual(12.0, Point.dot_product(p2,p1))
+        self.assertEqual(29.5, Point.dot_product(p2,p3))
+
+    def test_norm(self):
+        p3 = Point(3.5, 6.0)
+        self.assertEqual(69462, int(p3.norm() * 1e4))
+
+    def test_angle_between_segments_method(self):
+        p1 = Point(2.0, 1.0)
+        p2 = Point(5.0, 2.0)
+        p3 = Point(3.5, 6.0)
+        self.assertEqual(9575, int(Point.angle_between_segments(p2,p1,p3) * 1e4))
+
+    def test_min_y_coordinate(self):
+        p1 = Point(2.0, 1.0)
+        p2 = Point(5.0, 2.0)
+        p5 = Point(1.0, 1.0)
+        p3 = Point(3.5, 6.0)
+        p4 = Point(0.1, 16)
+        set_of_points = [p4, p2, p1, p3, p5]
+        self.assertEqual(4, Point.min_y_coordinate(set_of_points))
+
+    def test_min_graham_scan(self):
+        p1 = Point(2.0, 1.0)
+        p2 = Point(5.0, 2.0)
+        p5 = Point(1.0, 1.0)
+        p3 = Point(3.5, 6.0)
+        p4 = Point(0.1, 16)
+        set_of_points = [p4, p2, p1, p3, p5]
+        new_point = Point.graham_scan(set_of_points)
+        self.assertEqual(1.0, new_point.x)
+        self.assertEqual(1.0, new_point.y)
+
 if __name__ == '__main__':
     unittest.main()
